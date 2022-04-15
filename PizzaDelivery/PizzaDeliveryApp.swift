19b32cbf13220baab6f07e8df303141e2a4605cf
Sherislam <sherislam02@yandex.ru>
2022-04-15 12:07:39 +0600

    Initial Commit

---
+++ PizzaDelivery/PizzaDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  PizzaDeliveryApp.swift
+//  PizzaDelivery
+//
+//  Created by sher on 15/4/22.
+//
+
+import SwiftUI
+
+@main
+struct PizzaDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
